14a0acd2fd720fc494760684ab3d49efb4929370
alcander_liu <chiawei.liu.1201@gmail.com>
2018-01-11 13:40:40 +0800

    rough frame built

---
+++ main.js
@@ -0,0 +1,29 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 1600, height: 900});
+  mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocal: 'file',
+      slashes: true
+    })
+  )
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin')
+    app.quit();
+})
+
+app.on('activate', function () {
+  if (mainWindow === null)
+    createWindow();
+})
